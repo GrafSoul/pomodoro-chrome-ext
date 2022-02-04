9e69398fd83c59e19b0677bdbd63c22961f4a456
GrafSoul <grafsoul@gmail.com>
2022-02-04 17:40:16 +0500

    feat: Added timer in the extension

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,66 @@
-const tasks = [];
+let tasks = [];
+
+const startTimerBtn = document.getElementById("start-timer-btn");
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? "Pause Timer"
+          : "Start Timer";
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    }
+  );
+});
 
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
 
-function addTask() {
-  const tasksNum = tasks.length;
-  tasks.push("");
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
 
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
+
+function renderTask(tasksNum) {
   const taskRow = document.createElement("div");
 
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task...";
+  text.value = tasks[tasksNum];
   text.addEventListener("change", () => {
     tasks[tasksNum] = text.value;
+    saveTasks();
   });
 
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
   deleteBtn.value = "X";
   deleteBtn.addEventListener("click", () => {
-    tasks.splice(tasksNum, 1);
+    deleteTask(tasksNum);
+    saveTasks();
   });
 
   taskRow.appendChild(text);
@@ -28,4 +68,23 @@ function addTask() {
 
   const taskContainer = document.getElementById("task-container");
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const tasksNum = tasks.length;
+  tasks.push("");
+  renderTask(tasksNum);
+}
+
+function deleteTask(tasksNum) {
+  tasks.splice(tasksNum, 1);
+  renderTasks();
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.textContent = "";
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
 }
